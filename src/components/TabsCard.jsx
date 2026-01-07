b4ed32568800d4dc175617015c7ad9778bf1eb12
Felipe Becker <ofelipebecker@proton.me>
2026-01-07 15:32:14 -0300

    feat: add controlled tabs with state management to TabsCard
    Issue: DCD-2601A

--- src/components/TabsCard.jsx
+++ src/components/TabsCard.jsx
@@ -1,8 +1,28 @@
+import { useState } from 'react';
+
 import Card from 'react-bootstrap/Card';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
 const TabsCard = () => {
+  const [key, setKey] = useState('home');
+
   return (
-    <Card body className="border-0 rounded-2 shadow-sm"></Card>
+    <Card body className="border-0 rounded-2 shadow-sm">
+      <Tabs
+        id="controlled-tab-example"
+        activeKey={key}
+        onSelect={(k) => setKey(k)}
+        className="mb-3"
+      >
+        <Tab eventKey="home" title="Dados Móveis">
+          Dados Móveis
+        </Tab>
+        <Tab eventKey="profile" title="Voz">
+          Voz
+        </Tab>
+      </Tabs>
+    </Card>
   )
 }
 
