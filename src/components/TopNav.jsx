d3808dd33b6dda853f847941567882979d94df1d
Felipe Becker <ofelipebecker@proton.me>
2026-02-22 14:54:47 -0300

    feat: create TopNav component with logo integration
    Issue: DCD-2601A

---
+++ src/components/TopNav.jsx
@@ -0,0 +1,22 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+import logo from './../assets/images/logo-horizontal-purple-en.svg';
+
+const TopNav = () => {
+  return (
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand href="/">
+          <img
+            src={logo}
+            className='max-h-50px'
+            alt='logo'
+          />
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default TopNav;
