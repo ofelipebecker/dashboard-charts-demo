669d647f01a839bb8418a49898e211d4b05ed357
Felipe Becker <ofelipebecker@proton.me>
2026-02-25 17:25:42 -0300

    feat: redesign TopNav with vertical logo and external link buttons
    Issue: DCD-2601A

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,15 +1,34 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
 
-import logo from './../assets/images/logo-horizontal-purple-en.svg';
+import logo from './../assets/images/logo-vertical-purple-en.svg';
 
 const TopNav = () => {
   return (
-    <Navbar expand='lg' className='mt-5'>
-      <Container className='p-0'>
-        <Navbar.Brand href='/'>
-          <img src={logo} className='max-h-50px' alt='logo' />
-        </Navbar.Brand>
+    <Navbar expand='lg' className='mt-4 flex-column'>
+      <Container className='justify-content-center'>
+        <img src={logo} className='max-h-100px' alt='logo' />
+      </Container>
+      <Container className='justify-content-center mt-3'>
+        <Button
+          href='https://felipebecker.com/'
+          variant='link'
+          size='lg'
+          target='_blank'
+          title='Open in new tab'
+        >
+          Portfolio
+        </Button>
+        <Button
+          href='https://github.com/ofelipebecker'
+          variant='link'
+          size='lg'
+          target='_blank'
+          title='Open in new tab'
+        >
+          Github
+        </Button>
       </Container>
     </Navbar>
   );
