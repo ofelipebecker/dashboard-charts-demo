7d5ed144d8da97930b9c9a9d9beadbee7158b765
Felipe Becker <ofelipebecker@proton.me>
2026-02-25 19:30:56 -0300

    feat: extract mobile chart data to separate configuration file
    Issue: DCD-2601A

---
+++ src/data/mobileChartData.js
@@ -0,0 +1,6 @@
+export const mobileChartData = {
+  label: 'Monthly consumption in gigabytes',
+  chartData: [110, 30, 80, 10, 60, 120, 40, 100, 20, 70, 90, 50],
+  rgbColor: '255, 176, 0',
+  yAxisTitle: 'Gigabytes',
+};
