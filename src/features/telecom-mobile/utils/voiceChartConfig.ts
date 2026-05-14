78787bcde0ac88c88482bfeba19b0d7dc276daa5
Felipe Becker <ofelipebecker@proton.me>
2026-05-14 15:22:14 -0300

    feat: create voiceChartConfig.ts
    Issue: DCD-2605A

---
+++ src/features/telecom-mobile/utils/voiceChartConfig.ts
@@ -0,0 +1,6 @@
+export const voiceChartConfig = {
+  label: 'Monthly consumption in Minutes',
+  chartData: [87, 42, 175, 23, 76, 54, 98, 31, 105, 12, 67, 91],
+  rgbColor: '120, 94, 240',
+  yAxisTitle: 'Minutes',
+};
