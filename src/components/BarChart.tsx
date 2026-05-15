26619603411676a2a27dba59f7956febe024dbc0
Felipe Becker <ofelipebecker@proton.me>
2026-05-15 12:52:06 -0300

    feat: create BarChart.tsx
    Issue: DCD-2605A

---
+++ src/components/BarChart.tsx
@@ -0,0 +1,116 @@
+import { memo } from 'react';
+
+import { Bar } from 'react-chartjs-2';
+import {
+  type ChartOptions,
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+import 'chartjs-adapter-dayjs-4';
+import dayjs from 'dayjs';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+type chartConfig = {
+  label: string;
+  chartData: number[];
+  rgbColor: string;
+  yAxisTitle: string;
+};
+
+type BarChartProps = {
+  period: string;
+  chartConfig: chartConfig;
+};
+
+const BarChart = ({ period, chartConfig }: BarChartProps) => {
+  const monthsCount = parseInt(period);
+
+  const { label, chartData, rgbColor, yAxisTitle } = chartConfig;
+
+  const generateLabels = () => {
+    const currentDate = dayjs();
+    const startDate = currentDate.subtract(monthsCount - 1, 'month');
+
+    return Array.from({ length: monthsCount }, (_, index) =>
+      startDate.add(index, 'month').format('MMM/YYYY')
+    );
+  };
+
+  const labels = generateLabels();
+
+  const getDataForPeriod = () => {
+    switch (monthsCount) {
+      case 3:
+        return chartData.slice(-3);
+      case 6:
+        return chartData.slice(-6);
+      case 12:
+      default:
+        return chartData;
+    }
+  };
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label,
+        data: getDataForPeriod(),
+        backgroundColor: `rgba(${rgbColor}, 0.5)`,
+        borderColor: `rgb(${rgbColor})`,
+        borderWidth: 2,
+      },
+    ],
+  };
+
+  const options: ChartOptions<'bar'> = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        align: 'start',
+        labels: {
+          boxHeight: 15,
+          boxWidth: 15,
+        },
+        position: 'top',
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: yAxisTitle,
+        },
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return (
+    <div style={{ width: '100%', minHeight: '300px', position: 'relative' }}>
+      <Bar options={options} data={data} />
+    </div>
+  );
+};
+
+export default memo(BarChart);
