6e195378a70b328d780cb4154ccfdd19fd4e8c8b
Felipe Becker <ofelipebecker@proton.me>
2026-05-22 16:25:11 -0300

    refactor: extract treemap logic from DevicesByStateCard to chart config
    Issue: DCD-2605A

--- src/features/devices/components/DevicesByStateCard.jsx
+++ src/features/devices/components/DevicesByStateCard.jsx
@@ -1,125 +1,23 @@
 import CommonCard from '../../../components/CommonCard';
 import TreemapChart from '../../../components/TreemapChart';
-import { devicesByStateChartData } from '../utils/devicesByStateChartConfig';
+import {
+  devicesByStateChartData,
+  buttonText,
+  groups,
+  treemapCallbacks,
+} from '../utils/devicesByStateChartConfig';
 
 const DevicesByStateCard = () => {
-  const colors = {
-    main: '170, 68, 153', // #AA4499
-    groups: {
-      North: '100, 143, 255', // #648FFF
-      Northeast: '120, 94, 240', // #785EF0
-      Southeast: '220, 38, 127', // #DC267F
-      South: '254, 97, 0', // #FE6100
-      'Central-West': '255, 176, 0', // #FFB000
-    },
-  };
-
-  const maxTotal = Math.max(
-    ...devicesByStateChartData.map((state) => state.total)
-  );
-
-  const setCellBgColor = (ctx, activeGroup) => {
-    const treemapCell = ctx.raw;
-    const cellData = treemapCell?._data;
-    const isGroupedByRegion = activeGroup === 'alternate';
-
-    if (!cellData) return `rgba(${colors.main}, 0.5)`;
-
-    if (!isGroupedByRegion) {
-      const opacity = 0.3 + (cellData.total / maxTotal) * 0.9;
-      return `rgba(${colors.main}, ${opacity})`;
-    }
-
-    if (isGroupedByRegion)
-      return `rgba(${colors.groups[cellData.region]}, 0.7)`;
-  };
-
-  const formatLabel = (ctx) => {
-    const cellData = ctx.raw._data;
-
-    return [cellData.abbreviation, cellData.total];
-  };
-
-  const getTooltipCallbacks = (activeGroup) => ({
-    title: (ctx) => {
-      const cellData = ctx[0].raw._data;
-      const isGroupedByRegion = activeGroup === 'alternate';
-
-      if (isGroupedByRegion) {
-        return `Region: ${cellData.region} (Total: ${cellData.total} devices)`;
-      }
-
-      return `${cellData.children[0].name}: ${cellData.total} devices`;
-    },
-    label: function hideTooltipColorBox() {
-      return '';
-    },
-    afterBody: (ctx) => {
-      const cellData = ctx[0].raw._data;
-      const isGroupedByRegion = activeGroup === 'alternate';
-
-      const sumDeviceTotals = (acc, stateData) => ({
-        desktops: acc.desktops + stateData.devices.desktops,
-        laptops: acc.laptops + stateData.devices.laptops,
-        smartphones: acc.smartphones + stateData.devices.smartphones,
-        tablets: acc.tablets + stateData.devices.tablets,
-      });
-
-      const initialDeviceTotals = {
-        desktops: 0,
-        laptops: 0,
-        smartphones: 0,
-        tablets: 0,
-      };
-
-      const getRegionsTotals = (regionStatesData) => {
-        return regionStatesData.reduce(sumDeviceTotals, initialDeviceTotals);
-      };
-
-      const generateTooltipBody = (devices) => {
-        return [
-          `• Desktops: ${devices.desktops};`,
-          `• Laptops: ${devices.laptops};`,
-          `• Smartphones: ${devices.smartphones};`,
-          `• Tablets: ${devices.tablets};`,
-        ];
-      };
-
-      if (!isGroupedByRegion) {
-        const stateDevices = cellData.children[0].devices;
-
-        return generateTooltipBody(stateDevices);
-      }
-
-      if (isGroupedByRegion) {
-        const regionStatesData = cellData.children;
-        const regionDevices = getRegionsTotals(regionStatesData);
-
-        return generateTooltipBody(regionDevices);
-      }
-    },
-  });
-
   const cardTitles = {
     main: 'Devices by State | Brazil',
     secondary:
       'Demo: Device distribution of a hypothetical company across Brazilian states and regions.',
   };
 
-  const groups = {
-    default: ['abbreviation'],
-    alternate: ['region', 'abbreviation'],
-  };
-
-  const buttonText = {
-    default: 'Show Regions',
-    alternate: 'Hide Regions',
-  };
-
   const devicesByStateChart = (
     <TreemapChart
       rawChartData={devicesByStateChartData}
-      chartCallbacks={{ formatLabel, getTooltipCallbacks, setCellBgColor }}
+      chartCallbacks={treemapCallbacks}
       groups={groups}
       buttonText={buttonText}
     />
--- src/features/devices/utils/devicesByStateChartConfig.js
+++ src/features/devices/utils/devicesByStateChartConfig.js
@@ -12,3 +12,115 @@ export const devicesByStateChartData = devicesByStateData.map((state) => ({
     tablets: state.devices.tablets,
   },
 }));
+
+const colors = {
+  main: '170, 68, 153', // #AA4499
+  groups: {
+    North: '100, 143, 255', // #648FFF
+    Northeast: '120, 94, 240', // #785EF0
+    Southeast: '220, 38, 127', // #DC267F
+    South: '254, 97, 0', // #FE6100
+    'Central-West': '255, 176, 0', // #FFB000
+  },
+};
+
+export const buttonText = {
+  default: 'Show Regions',
+  alternate: 'Hide Regions',
+};
+
+export const groups = {
+  default: ['abbreviation'],
+  alternate: ['region', 'abbreviation'],
+};
+
+const maxTotal = Math.max(
+  ...devicesByStateChartData.map((state) => state.total)
+);
+
+const setCellBgColor = (ctx, activeGroup) => {
+  const treemapCell = ctx.raw;
+  const cellData = treemapCell?._data;
+  const isGroupedByRegion = activeGroup === 'alternate';
+
+  if (!cellData) return `rgba(${colors.main}, 0.5)`;
+
+  if (!isGroupedByRegion) {
+    const opacity = 0.3 + (cellData.total / maxTotal) * 0.9;
+    return `rgba(${colors.main}, ${opacity})`;
+  }
+
+  if (isGroupedByRegion) return `rgba(${colors.groups[cellData.region]}, 0.7)`;
+};
+
+const formatLabel = (ctx) => {
+  const cellData = ctx.raw._data;
+
+  return [cellData.abbreviation, cellData.total];
+};
+
+const getTooltipCallbacks = (activeGroup) => ({
+  title: (ctx) => {
+    const cellData = ctx[0].raw._data;
+    const isGroupedByRegion = activeGroup === 'alternate';
+
+    if (isGroupedByRegion) {
+      return `Region: ${cellData.region} (Total: ${cellData.total} devices)`;
+    }
+
+    return `${cellData.children[0].name}: ${cellData.total} devices`;
+  },
+  label: function hideTooltipColorBox() {
+    return '';
+  },
+  afterBody: (ctx) => {
+    const cellData = ctx[0].raw._data;
+    const isGroupedByRegion = activeGroup === 'alternate';
+
+    const sumDeviceTotals = (acc, stateData) => ({
+      desktops: acc.desktops + stateData.devices.desktops,
+      laptops: acc.laptops + stateData.devices.laptops,
+      smartphones: acc.smartphones + stateData.devices.smartphones,
+      tablets: acc.tablets + stateData.devices.tablets,
+    });
+
+    const initialDeviceTotals = {
+      desktops: 0,
+      laptops: 0,
+      smartphones: 0,
+      tablets: 0,
+    };
+
+    const getRegionsTotals = (regionStatesData) => {
+      return regionStatesData.reduce(sumDeviceTotals, initialDeviceTotals);
+    };
+
+    const generateTooltipBody = (devices) => {
+      return [
+        `• Desktops: ${devices.desktops};`,
+        `• Laptops: ${devices.laptops};`,
+        `• Smartphones: ${devices.smartphones};`,
+        `• Tablets: ${devices.tablets};`,
+      ];
+    };
+
+    if (!isGroupedByRegion) {
+      const stateDevices = cellData.children[0].devices;
+
+      return generateTooltipBody(stateDevices);
+    }
+
+    if (isGroupedByRegion) {
+      const regionStatesData = cellData.children;
+      const regionDevices = getRegionsTotals(regionStatesData);
+
+      return generateTooltipBody(regionDevices);
+    }
+  },
+});
+
+export const treemapCallbacks = {
+  setCellBgColor,
+  formatLabel,
+  getTooltipCallbacks,
+};
