971fa568bda78720ea0c7fcbf617de53b43437d7
Felipe Becker <ofelipebecker@proton.me>
2026-05-23 12:39:27 -0300

    feat: add colors and maxTotal to devicesByStateChartConfig.ts
    Issue: DCD-2605A

--- src/features/devices/utils/devicesByStateChartConfig.ts
+++ src/features/devices/utils/devicesByStateChartConfig.ts
@@ -12,3 +12,18 @@ export const devicesByStateChartData = devicesByStateData.map((state) => ({
     tablets: state.devices.tablets,
   },
 }));
+
+const colors = {
+  main: '170, 68, 153', // #AA4499
+  regions: {
+    North: '100, 143, 255', // #648FFF
+    Northeast: '120, 94, 240', // #785EF0
+    Southeast: '220, 38, 127', // #DC267F
+    South: '254, 97, 0', // #FE6100
+    'Central-West': '255, 176, 0', // #FFB000
+  },
+};
+
+const maxTotal = Math.max(
+  ...devicesByStateChartData.map((state) => state.total)
+);
