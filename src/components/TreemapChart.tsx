fad4cee859e043496c289ffc825f18f362ca7642
Felipe Becker <ofelipebecker@proton.me>
2026-05-25 10:10:29 -0300

    feat: add chartCallbacks prop to TreemapChart.tsx
    Issue: DCD-2605A

--- src/components/TreemapChart.tsx
+++ src/components/TreemapChart.tsx
@@ -1,14 +1,20 @@
 import {
   type DevicesByStateData,
+  type DevicesByStateChartCallbacks,
   type DevicesByStateChartGroupingConfig,
 } from '../features/devices/utils/devicesByStateChartConfig.ts';
 
 type TreemapChartProps = {
   rawChartData: DevicesByStateData[];
+  chartCallbacks: DevicesByStateChartCallbacks;
   groupingConfig: DevicesByStateChartGroupingConfig;
 };
 
-const TreemapChart = ({ rawChartData, groupingConfig }: TreemapChartProps) => {
+const TreemapChart = ({
+  rawChartData,
+  chartCallbacks,
+  groupingConfig,
+}: TreemapChartProps) => {
   return <div></div>;
 };
 
