deb7261b7dbf9a2704593fe665ad982b6eab12d0
Felipe Becker <ofelipebecker@proton.me>
2026-05-25 09:44:57 -0300

    feat: add DevicesByStateChartGroupingConfig type export
    Issue: DCD-2605A

--- src/features/devices/utils/devicesByStateChartConfig.ts
+++ src/features/devices/utils/devicesByStateChartConfig.ts
@@ -69,6 +69,9 @@ export const devicesByStateChartGroupingConfig = {
   },
 };
 
+export type DevicesByStateChartGroupingConfig =
+  typeof devicesByStateChartGroupingConfig;
+
 export const devicesByStateChartCallbacks = {
   setCellBgColor: (
     ctx: TreemapScriptableContext,
