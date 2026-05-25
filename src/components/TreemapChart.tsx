40fad1fc43b3bcfceb87a8d8e61393a95e2a6fdd
Felipe Becker <ofelipebecker@proton.me>
2026-05-25 11:35:06 -0300

    feat: add toggle button and group state logic to TreemapChart.tsx
    Issue: DCD-2605A

--- src/components/TreemapChart.tsx
+++ src/components/TreemapChart.tsx
@@ -1,8 +1,11 @@
+import { useRef, useState } from 'react';
 import {
   type DevicesByStateData,
   type DevicesByStateChartCallbacks,
   type DevicesByStateChartGroupingConfig,
 } from '../features/devices/utils/devicesByStateChartConfig.ts';
+
+import Button from 'react-bootstrap/Button';
 
 type TreemapChartProps = {
   rawChartData: DevicesByStateData[];
@@ -15,7 +18,33 @@ const TreemapChart = ({
   chartCallbacks,
   groupingConfig,
 }: TreemapChartProps) => {
-  return <div></div>;
+  const { buttonText } = groupingConfig;
+
+  const chartRef = useRef(null);
+  const [activeGroup, setActiveGroup] = useState('default');
+
+  const handleToggle = () => {
+    if (!chartRef.current) return;
+
+    const newGroup = activeGroup === 'default' ? 'alternate' : 'default';
+    setActiveGroup(newGroup);
+  };
+
+  const buttonLabel =
+    activeGroup === 'default' ? buttonText.default : buttonText.alternate;
+
+  return (
+    <div>
+      <Button
+        className='my-4'
+        onClick={handleToggle}
+        size='lg'
+        variant='warning'
+      >
+        {buttonLabel}
+      </Button>
+    </div>
+  );
 };
 
 export default TreemapChart;
